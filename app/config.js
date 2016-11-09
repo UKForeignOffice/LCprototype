2239ac70e7b823ef6991dd8f8d497b8c77d9c8be
laura czapiewski <laura.czapiewski@digital.fco.gov.uk>
2016-11-09 14:05:54 +0000

    logic added

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Laura prototype',
+  serviceName: 'Emergency Travel Documents Prototype',
 
   // Default port that prototype runs on
   port: '3000',
